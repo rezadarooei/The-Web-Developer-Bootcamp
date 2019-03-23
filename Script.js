3e7445fd775908b23df4f0d7ca7758ad5f47778a
DESKTOP-PDHHLPS\Reza <reza.darooei@gmail.com>
2019-03-23 18:44:04 +0430

    Basic Html and JavaScript

---
+++ Script.js
@@ -0,0 +1,65 @@
+/*console.log("Hello world");
+var Year=2016-26;
+console.log(Year)
+//if else statement
+var name="John";
+var Age=25;
+var Ismarried=false;
+if(Ismarried==true){
+	console.log("hurraaa");
+}
+else{
+	console.log("you will marrid soon")
+}
+
+var Job=prompt("What is John Job?");
+switch(Job){
+	case "Teachear":
+	
+		console.log("He is teachear");
+		break;
+	case "Driver":
+			console.log("He is Driver");
+			break;
+	 default:
+			console.log("He did something else");
+			break;
+	}
+*/
+/*
+var HieghtMilad=175;
+var HieghtReza=180;
+var AgeMilad=27;
+var AgeReza=27;
+var ScoreMilad=HieghtMilad+5*AgeMilad;
+var ScoreReza=HieghtReza+5*AgeReza;
+var WinnerScore=prompt("winner score?");
+if(ScoreReza>ScoreMilad){
+	console.log("Reza win")
+}
+else{
+	console.log("milad win")
+}
+*/
+// function CalculateAge(BirthAge){
+// 	var age=2019-BirthAge;
+// 	return age;
+// }
+// var BirhofJohn=1991;
+// var JohnAGe=CalculateAge(BirhofJohn);
+// console.log(JohnAGe);
+var Reza={
+	Name:"Reza",
+	Famiy:"Darooei",
+	Age:27,
+	Job:"Student",
+	IsmARRIED:false
+};
+
+var Melisa=new Object();
+
+Melisa.Age=28;
+Melisa.Sex="Wmoan";
+console.log(Melisa);
+
+
